3d98079e044b4959d7f24ab5449557ed0fc3f852
smykdianaa <dsmyk2023@itcollege.lviv.ua>
2025-10-06 15:50:48 +0300

    lab1

---
+++ lab1/lab1.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+name = "Diana"
+location = "Lviv"
+
+print(f"{name} start programming at {datetime.now()}. {location} is the best city!")
+print("Python працює правильно!")
--- lab1/my_first_app.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-name = "Diana"
-location = "Lviv"
-
-print(f"{name} start programming at {datetime.now()}. {location} is the best city!")
